7cfbfcaaca79c79f943091dae1f322152f8acc07
afzalmardiansyah <mardyanachal@gmail.com>
2019-01-20 23:31:18 +0700

    Penambahan Data

--- src/main/java/com/belajar/spring/dao/BaseDAOi.java
+++ src/main/java/com/belajar/spring/dao/BaseDAOi.java
@@ -1,7 +1,10 @@
 package com.belajar.spring.dao;
 import java.util.List;
 
-
+/**
+ * Created by afzalmardiansyah on 20/01/19
+ * @param <U>
+ */
 public interface BaseDAOi<U> {
     U save(U param);
 
